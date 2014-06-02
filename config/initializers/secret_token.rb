1e30dc2621233e31b49ddadfa0f8f519f3cad010
sai <sai@sai-300E4Z-300E5Z-300E7Z.(none)>
2014-06-02 21:28:12 +0530

    s

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'dc2b0b5bb9c406aae9b334e51be5132de1382a4f26ce83f451a7890b543d46c6c778362144c9038e8c0daa7ef2e55c7fd4979c32066426d614e23d385885e309'
